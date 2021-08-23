06427904f89cf5f2141a1bef6dc9567cbb66ee6e
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-08-23 19:01:42 -0300

    adiciona exemplo de state em componentes de classe

--- examples/src/components/lottery/Lottery.js
+++ examples/src/components/lottery/Lottery.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, TextInput, StyleSheet} from 'react-native';
 
 import style from '../../style';
 
 export default class Lottery extends Component {
+  state = {
+    quantity: this.props.quantity,
+  };
+
+  setQuantity = quantity => {
+    this.setState({quantity});
+  };
+
   render() {
     return (
       <>
         <Text style={style.largeFont}>Gerador Mega-Sena</Text>
         <Text style={style.mediumFont}>
-          Quantidade de números: {this.props.quantity}
+          Quantidade de números: {this.state.quantity}
         </Text>
+        <TextInput
+          keyboardType="numeric"
+          style={localStyle.textInput}
+          placeholder="Quantidade de números"
+          value={this.state.quantity}
+          onChangeText={this.setQuantity}
+        />
       </>
     );
   }
 }
+
+const localStyle = StyleSheet.create({
+  textInput: {
+    borderBottomWidth: 1,
+  },
+});
