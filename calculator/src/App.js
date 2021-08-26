2c66c9d0c83e544bc35b19ddc1afb711b84dec47
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-08-26 17:50:44 -0300

    adiciona componente de botão

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,6 +1,38 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
-export default () => {
-  return <Text>Teste 123</Text>;
-};
+import Button from './components/Button';
+
+export default () => (
+  <View style={styles.container}>
+    <View style={styles.buttons}>
+      <Button label="AC" />
+      <Button label="/" />
+      <Button label="7" />
+      <Button label="8" />
+      <Button label="9" />
+      <Button label="*" />
+      <Button label="4" />
+      <Button label="5" />
+      <Button label="6" />
+      <Button label="-" />
+      <Button label="1" />
+      <Button label="2" />
+      <Button label="3" />
+      <Button label="+" />
+      <Button label="0" />
+      <Button label="." />
+      <Button label="=" />
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+});
