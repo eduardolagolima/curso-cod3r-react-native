9334ddd1cd480b929d7c96c41b2bd25b77d41555
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-08-26 18:07:30 -0300

    adiciona componente do visor

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 
 import Button from './components/Button';
+import Display from './components/Display';
 
-export default () => (
-  <View style={styles.container}>
-    <View style={styles.buttons}>
-      <Button label="AC" />
-      <Button label="/" />
-      <Button label="7" />
-      <Button label="8" />
-      <Button label="9" />
-      <Button label="*" />
-      <Button label="4" />
-      <Button label="5" />
-      <Button label="6" />
-      <Button label="-" />
-      <Button label="1" />
-      <Button label="2" />
-      <Button label="3" />
-      <Button label="+" />
-      <Button label="0" />
-      <Button label="." />
-      <Button label="=" />
+export default () => {
+  const [displayValue, setDisplayValue] = useState('0');
+
+  return (
+    <View style={styles.container}>
+      <Display value={displayValue} />
+      <View style={styles.buttons}>
+        <Button label="AC" />
+        <Button label="/" />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="*" />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" />
+        <Button label="0" />
+        <Button label="." />
+        <Button label="=" />
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
