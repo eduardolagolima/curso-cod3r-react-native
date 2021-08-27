defb56983d347da222f003c2701b86feb9a1ac1f
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-08-27 09:59:14 -0300

    adiciona ação e estilo nos botões

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {StyleSheet, Text, Dimensions, TouchableHighlight} from 'react-native';
 
-export default ({onClick, label}) => {
+export default ({onClick, label, double, triple, operation}) => {
+  const buttonStyles = [
+    styles.button,
+    ...(double ? [styles.doubleButton] : []),
+    ...(triple ? [styles.tripleButton] : []),
+    ...(operation ? [styles.operationButton] : []),
+  ];
+
   return (
     <>
-      <TouchableHighlight onPress={onClick}>
-        <Text style={styles.button}>{label}</Text>
+      <TouchableHighlight onPress={() => onClick(label)}>
+        <Text style={buttonStyles}>{label}</Text>
       </TouchableHighlight>
     </>
   );
@@ -21,5 +28,15 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 1,
     borderColor: '#888',
+  },
+  doubleButton: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  tripleButton: {
+    width: (Dimensions.get('window').width / 4) * 3,
+  },
+  operationButton: {
+    color: '#fff',
+    backgroundColor: '#fa8231',
   },
 });
