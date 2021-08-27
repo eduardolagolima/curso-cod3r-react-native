73dce4f75800557d95b53a3cc46e559b675bf258
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-08-27 15:53:10 -0300

    adiciona lógica da calculadora

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,49 +1,104 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {StyleSheet, View} from 'react-native';
 
 import Button from './components/Button';
 import Display from './components/Display';
 
-export default () => {
-  const [displayValue, setDisplayValue] = useState('0');
-
-  const addDigit = digit => {
-    setDisplayValue(digit);
-  };
-
-  const clearMemory = () => {
-    setDisplayValue('0');
-  };
-
-  const setOperation = operation => {
-    console.warn(operation);
-  };
-
-  return (
-    <View style={styles.container}>
-      <Display value={displayValue} />
-      <View style={styles.buttons}>
-        <Button label="AC" triple onClick={clearMemory} />
-        <Button label="/" operation onClick={setOperation} />
-        <Button label="7" onClick={addDigit} />
-        <Button label="8" onClick={addDigit} />
-        <Button label="9" onClick={addDigit} />
-        <Button label="*" operation onClick={setOperation} />
-        <Button label="4" onClick={addDigit} />
-        <Button label="5" onClick={addDigit} />
-        <Button label="6" onClick={addDigit} />
-        <Button label="-" operation onClick={setOperation} />
-        <Button label="1" onClick={addDigit} />
-        <Button label="2" onClick={addDigit} />
-        <Button label="3" onClick={addDigit} />
-        <Button label="+" operation onClick={setOperation} />
-        <Button label="0" double onClick={addDigit} />
-        <Button label="." onClick={addDigit} />
-        <Button label="=" operation onClick={setOperation} />
-      </View>
-    </View>
-  );
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
 };
+
+export default class App extends React.Component {
+  state = {...initialState};
+
+  addDigit = digit => {
+    const clearDisplay =
+      this.state.displayValue === '0' || this.state.clearDisplay;
+
+    if (
+      digit === '.' &&
+      !clearDisplay &&
+      this.state.displayValue.includes('.')
+    ) {
+      return;
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + digit;
+
+    this.setState({displayValue, clearDisplay: false});
+
+    if (digit !== '.') {
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+
+      values[this.state.current] = newValue;
+
+      this.setState({values});
+    }
+  };
+
+  clearMemory = () => {
+    this.setState({...initialState});
+  };
+
+  setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({operation, current: 1, clearDisplay: true});
+    } else {
+      const equals = operation === '=';
+      const values = [...this.state.values];
+
+      try {
+        /* eslint-disable no-eval */
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+          <Button label="AC" triple onClick={this.clearMemory} />
+          <Button label="/" operation onClick={this.setOperation} />
+          <Button label="7" onClick={this.addDigit} />
+          <Button label="8" onClick={this.addDigit} />
+          <Button label="9" onClick={this.addDigit} />
+          <Button label="*" operation onClick={this.setOperation} />
+          <Button label="4" onClick={this.addDigit} />
+          <Button label="5" onClick={this.addDigit} />
+          <Button label="6" onClick={this.addDigit} />
+          <Button label="-" operation onClick={this.setOperation} />
+          <Button label="1" onClick={this.addDigit} />
+          <Button label="2" onClick={this.addDigit} />
+          <Button label="3" onClick={this.addDigit} />
+          <Button label="+" operation onClick={this.setOperation} />
+          <Button label="0" double onClick={this.addDigit} />
+          <Button label="." onClick={this.addDigit} />
+          <Button label="=" operation onClick={this.setOperation} />
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
