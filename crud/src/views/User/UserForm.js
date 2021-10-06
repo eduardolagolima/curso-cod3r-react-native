d15dbaed15a9df134a54752cdf1e902b86aabf4f
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-10-06 18:58:12 -0300

    adiciona campos no formulário de usuário

--- crud/src/views/User/UserForm.js
+++ crud/src/views/User/UserForm.js
@@ -1,7 +1,53 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, TextInput, StyleSheet, Button} from 'react-native';
 
-export default props => {
-  const {user} = props.route.params ?? {};
-  return <Text>{user?.name}</Text>;
+export default ({route, navigation}) => {
+  const [user, setUser] = useState(route.params?.user ? route.params.user : {});
+
+  return (
+    <View style={style.form}>
+      <Text style={style.label}>Nome</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={name => setUser({...user, name})}
+        placeholder="Informe o Nome"
+        value={user.name}
+      />
+      <Text style={style.label}>Email</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={email => setUser({...user, email})}
+        placeholder="Informe o E-mail"
+        value={user.email}
+      />
+      <Text style={style.label}>URL do Avatar</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+        placeholder="Informe a URL do Avatar"
+        value={user.avatarUrl}
+      />
+      <Button
+        title="Salvar"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  form: {
+    padding: 15,
+  },
+  label: {
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+  },
+});
