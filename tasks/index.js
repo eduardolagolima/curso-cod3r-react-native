69a5c7a618c4d1a0e55828a94bbe66af9a017721
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-10-10 17:11:52 -0300

    adiciona screen TaskList

--- tasks/index.js
+++ tasks/index.js
@@ -4,7 +4,9 @@
 
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import TaskList from './src/screens/TaskList';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TaskList);
+
+import './src/plugins/dayjs';
