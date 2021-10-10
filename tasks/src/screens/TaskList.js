40552d5aab6b308f3b9b86cbee649aa25ab09fe1
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-10-10 18:28:29 -0300

    adiciona FlatList na screen TaskList

--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, ImageBackground, StyleSheet} from 'react-native';
+import {Text, View, ImageBackground, StyleSheet, FlatList} from 'react-native';
 import dayjs from 'dayjs';
 
 import commonStyles from '../styles/common';
@@ -8,6 +8,23 @@ import todayImage from '../../assets/imgs/today.jpg';
 import Task from '../components/Task';
 
 export default class TaskList extends Component {
+  state = {
+    tasks: [
+      {
+        id: 1,
+        description: 'Comprar livro',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: 2,
+        description: 'Ler livro',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+    ],
+  };
+
   render() {
     const today = dayjs().format('ddd, D [de] MMMM');
 
@@ -20,12 +37,11 @@ export default class TaskList extends Component {
           </View>
         </ImageBackground>
         <View style={styles.taskList}>
-          <Task
-            description="Comprar livro"
-            estimateAt={new Date()}
-            doneAt={new Date()}
+          <FlatList
+            data={this.state.tasks}
+            keyExtractor={item => `${item.id}`}
+            renderItem={({item}) => <Task {...item} />}
           />
-          <Task description="Ler livro" estimateAt={new Date()} doneAt={null} />
         </View>
       </View>
     );
