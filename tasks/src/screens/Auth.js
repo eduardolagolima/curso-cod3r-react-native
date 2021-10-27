6f3f1febd63b7f573959747e2e91670b49c26d2e
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-10-27 11:55:41 -0300

    adiciona formulário na tela de autenticação

--- tasks/src/screens/Auth.js
+++ tasks/src/screens/Auth.js
@@ -1,15 +1,46 @@
 import React, {Component} from 'react';
-import {ImageBackground, Text, StyleSheet} from 'react-native';
+import {
+  ImageBackground,
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 import loginImage from '../../assets/imgs/login.jpg';
 
 import commonStyles from '../styles/common';
 
 export default class Auth extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
   render() {
     return (
       <ImageBackground source={loginImage} style={styles.background}>
         <Text style={styles.title}>Tasks</Text>
+        <View style={styles.formContainer}>
+          <TextInput
+            placeholder="E-mail"
+            value={this.state.email}
+            style={styles.textInput}
+            onChangeText={email => this.setState({email})}
+          />
+          <TextInput
+            placeholder="Senha"
+            value={this.state.password}
+            style={styles.textInput}
+            onChangeText={password => this.setState({password})}
+          />
+          <TouchableOpacity>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Entrar</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
     );
   }
@@ -21,6 +52,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     width: '100%',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#080',
+    marginTop: 10,
+    padding: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 20,
+  },
+  formContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    padding: 20,
+    width: '90%',
+  },
+  textInput: {
+    backgroundColor: '#fff',
+    marginTop: 10,
+    padding: 10,
   },
   title: {
     color: commonStyles.colors.secondary,
