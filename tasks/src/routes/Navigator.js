5ac8993467617adc9072dd5fa1a8f05c9302b8bc
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-10-30 23:04:16 -0300

    cria drawer de navegação para trocar a visualização

--- tasks/src/routes/Navigator.js
+++ tasks/src/routes/Navigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
@@ -7,15 +8,30 @@ import Auth from '../screens/Auth';
 import TaskList from '../screens/TaskList';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => (
+  <Drawer.Navigator screenOptions={{headerShown: false}}>
+    <Drawer.Screen name="Today" options={{title: 'Hoje'}}>
+      {props => <TaskList {...props} title="Hoje" daysAhead={0} />}
+    </Drawer.Screen>
+    <Drawer.Screen name="Tomorrow" options={{title: 'Amanhã'}}>
+      {props => <TaskList {...props} title="Amanhã" daysAhead={1} />}
+    </Drawer.Screen>
+    <Drawer.Screen name="Week" options={{title: 'Semana'}}>
+      {props => <TaskList {...props} title="Semana" daysAhead={7} />}
+    </Drawer.Screen>
+    <Drawer.Screen name="Month" options={{title: 'Mês'}}>
+      {props => <TaskList {...props} title="Mês" daysAhead={30} />}
+    </Drawer.Screen>
+  </Drawer.Navigator>
+);
 
 export default () => (
   <NavigationContainer>
-    <Stack.Navigator
-      initialRouteName="Auth"
-      screenOptions={{headerShown: false}}
-    >
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Auth" component={Auth} />
-      <Stack.Screen name="Home" component={TaskList} />
+      <Stack.Screen name="Home" component={DrawerNavigator} />
     </Stack.Navigator>
   </NavigationContainer>
 );
