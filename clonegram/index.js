8bb59be69b8dff142eddcfe6c781530c2eec1b77
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-11-08 16:46:24 -0300

    configura para a aplicação usar o redux

--- clonegram/index.js
+++ clonegram/index.js
@@ -1,6 +1,17 @@
+import React from 'react';
+
 import {AppRegistry} from 'react-native';
+
+import {Provider} from 'react-redux';
 
 import {name as appName} from './app.json';
 import Navigator from './src/routes/Navigator';
+import storeConfig from './src/store/storeConfig';
 
-AppRegistry.registerComponent(appName, () => Navigator);
+const Redux = () => (
+  <Provider store={storeConfig()}>
+    <Navigator />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => Redux);
